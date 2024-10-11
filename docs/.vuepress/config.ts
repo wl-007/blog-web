7ebf05c58d423eaf7cfc071440ec82d8cfa2d913
wl-new <wanglei@777.com>
2024-10-11 22:10:07 +0800

    初始化-css

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,81 @@
+import { defineConfig4CustomTheme, UserPlugins } from 'vuepress/config';
+const BASE='/blog-web/';
+
+const blogs=[
+	{
+		title: 'CSS',
+		children: [
+			{
+				title: 'CSS基础',
+				path: '/css/css基础.md',
+			},
+			{
+				title: 'CSS进阶',
+				path: '/css/css进阶.md',
+			},
+		]
+	}
+]
+
+export default defineConfig4CustomTheme({
+	locales: {
+		'/': {
+			lang: 'zh-CN',
+			title: 'web',
+			description: 'web文档',
+		},
+	},
+	base: BASE,
+	themeConfig: {
+		nav: [
+
+			...blogs.map(blog=>{
+				return {
+					text: blog.title,
+					items: blog.children.map(child=>{
+						return {
+							text: child.title,
+							link: child.path
+						}
+					})
+				}
+			})
+		],
+		sidebar: blogs.map(blog=>{
+			return {
+				text: blog.title,
+				children: blog.children.map(child=>{
+					return {
+						title: child.title,
+						path: child.path
+					}
+				})
+			}
+		}),
+		searchMaxSuggestions: 10,
+		
+	},
+	dest: 'dist',
+	plugins: <UserPlugins>[
+		[
+			'one-click-copy',
+			{
+				copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'],
+				copyMessage: '复制成功',
+				duration: 1000,
+				showInMobile: false,
+			},
+		],
+
+		[
+			'vuepress-plugin-zooming',
+			{
+				selector: '.theme-vdoing-content img:not(.no-zoom)',
+				options: {
+					bgColor: 'rgba(0,0,0,0.6)',
+				},
+			},
+		],
+	],
+	extraWatchFiles: ['.vuepress/config.ts'],
+});
---
+++ docs/index.md
@@ -0,0 +1,6 @@
+---
+home: true
+heroText: 前端文档记录
+
+actionText: 立刻进入 →
+actionLink: /css/css基础.md
